54c464b6b12b5eb6e63e0a87b21c0cae0ec8e177
saroshmirza <mirza.salman.sarosh@gmail.com>
2021-03-25 22:37:30 +0100

    rev

--- _src/index.js
+++ _src/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import Tobi from 'rqrauhvmra__tobi'
 const tobi = new Tobi()
 
-Remove the two following lines to remove the product hunt floating prompt
+// Remove the two following lines to remove the product hunt floating prompt
 import FloatingPrompt from 'producthunt-floating-prompt'
 FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
 
